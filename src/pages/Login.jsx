59b26e8eb0a3fafdbe5cf7e60b8fac1002213489
Youssef Nagy <youssefn899@gmail.com>
2026-07-27 23:46:39 +0300

    feat: replace social icons with Continue with Google button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,6 @@ import { notify } from "../utils/notify";
 import "./Login.css";
 
 import {
-    FaGoogle, FaFacebookF, FaApple,
     FaUser, FaEnvelope, FaLock, FaPhone, FaEye, FaEyeSlash
 } from "react-icons/fa";
 
@@ -130,16 +129,6 @@ const Login = () => {
         window.location.href = `${API_URL}/api/auth/google`;
     };
 
-    const handleFacebookAuth = (e) => {
-        e.preventDefault();
-        window.location.href = `${API_URL}/api/auth/facebook`;
-    };
-
-    const handleAppleAuth = (e) => {
-        e.preventDefault();
-        window.location.href = `${API_URL}/api/auth/apple`;
-    };
-
     return (
         <div className="login-body">
             <div className={`login-container ${isActive ? "active" : ""}`} id="container">
@@ -148,11 +137,15 @@ const Login = () => {
                 <div className="form-container sign-up">
                     <form onSubmit={submitRegister}>
                         <h1>Create Account</h1>
-                        <div className="social-icons">
-                            <a href="#" className="icon" title="Sign up with Google" onClick={handleGoogleAuth}><FaGoogle /></a>
-                            <a href="#" className="icon" title="Sign up with Facebook" onClick={handleFacebookAuth}><FaFacebookF /></a>
-                            <a href="#" className="icon" title="Sign up with Apple" onClick={handleAppleAuth}><FaApple /></a>
-                        </div>
+                        <button type="button" className="google-btn" onClick={handleGoogleAuth}>
+                            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262">
+                                <path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
+                                <path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
+                                <path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"></path>
+                                <path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
+                            </svg>
+                            Continue with Google
+                        </button>
                         <span>or use your email for registration</span>
                         <div className="input-wrapper">
                             <input type="text" placeholder="Name" ref={regNameRef} required />
@@ -194,11 +187,15 @@ const Login = () => {
                 <div className="form-container sign-in">
                     <form onSubmit={submitLogin}>
                         <h1>Sign In</h1>
-                        <div className="social-icons">
-                            <a href="#" className="icon" title="Sign in with Google" onClick={handleGoogleAuth}><FaGoogle /></a>
-                            <a href="#" className="icon" title="Sign in with Facebook" onClick={handleFacebookAuth}><FaFacebookF /></a>
-                            <a href="#" className="icon" title="Sign in with Apple" onClick={handleAppleAuth}><FaApple /></a>
-                        </div>
+                        <button type="button" className="google-btn" onClick={handleGoogleAuth}>
+                            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262">
+                                <path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
+                                <path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
+                                <path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"></path>
+                                <path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
+                            </svg>
+                            Continue with Google
+                        </button>
                         <span>or use your email password</span>
                         <div className="input-wrapper">
                             <input type="email" placeholder="Email" ref={loginEmailRef} required />
