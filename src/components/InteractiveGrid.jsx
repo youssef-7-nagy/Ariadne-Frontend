fc879eb172b3dff476c4972853cd2f5a4d338a6a
Youssef Nagy <youssefn899@gmail.com>
2026-09-18 13:51:11 +0300

    Decrease Kamena logo scale to 1.75

--- src/components/InteractiveGrid.jsx
+++ src/components/InteractiveGrid.jsx
@@ -215,7 +215,7 @@ export default function InteractiveGrid(props) {
                     margin: "0 auto",
                     userSelect: "none",
                     pointerEvents: "none",
-                    transform: logoSrc.includes('Kamena') ? 'scale(2.0)' : 'none',
+                    transform: logoSrc.includes('Kamena') ? 'scale(1.75)' : 'none',
                   }}
                 />
               )}
